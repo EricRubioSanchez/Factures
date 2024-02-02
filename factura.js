920ce235cb180d192027f6b2593faef94682a0c0
rauldmj3 <r.mingo@sapalomera.cat>
2024-02-02 16:41:08 +0100

    Formatar documento

--- factura.js
+++ factura.js
@@ -1,142 +1,154 @@
 "use strict";
-import {canviarCelda, crearTable, crearTable2, moureTaula,omplirDades,crearBotonsTaula} from "./table.js";
-import {crearElement,modificarElement,moureElement} from "./Exercici.js";
+import { canviarCelda, crearTable, crearTable2, moureTaula, omplirDades, crearBotonsTaula } from "./table.js";
+import { crearElement, modificarElement, moureElement } from "./Exercici.js";
 
 
 //Dialogo
-const facturaDia=document.getElementById("factura");
-const productosDia=document.getElementById("productos");
+const facturaDia = document.getElementById("factura");
+const productosDia = document.getElementById("productos");
 
 //Arrays
-const Facturas=[];
+const Facturas = [];
 
 class Factura {
-	constructor(id, data, nif, client, telefon, email, dte, iva, pagat,adreca,poblacio,productos) {
-		this.id= id;
-		this.data= data;
-		this.nif= nif;
-		this.client= client;
-		this.telefon= telefon;
-		this.email= email;
-		this.productos=productos;
-		this.dte= dte;
-		this.iva= iva;
-		this.pagat=pagat;
-		this.adreca=adreca;
-		this.poblacio=poblacio;
+	constructor(id, data, nif, client, telefon, email, dte, iva, pagat, adreca, poblacio, productos) {
+		this.id = id;
+		this.data = data;
+		this.nif = nif;
+		this.client = client;
+		this.telefon = telefon;
+		this.email = email;
+		this.productos = productos;
+		this.dte = dte;
+		this.iva = iva;
+		this.pagat = pagat;
+		this.adreca = adreca;
+		this.poblacio = poblacio;
 	}
 }
 class Articulo {
-	constructor(codi,article,unidad,preu){
-		this.codi= codi;
-		this.article= article;
-		this.unidad= unidad;
-		this.preu= preu;
+	constructor(codi, article, unidad, preu) {
+		this.codi = codi;
+		this.article = article;
+		this.unidad = unidad;
+		this.preu = preu;
 	}
 }
 
-function carregar(event){
+function carregar(event) {
 	let fileReader = new FileReader();
 	fileReader.readAsText(event.target.files[0]);
 	let jsonAsString;
-	fileReader.onload = function(event) {
-		jsonAsString=fileReader.result;
-		let json=JSON.parse(jsonAsString);
+	fileReader.onload = function (event) {
+		jsonAsString = fileReader.result;
+		let json = JSON.parse(jsonAsString);
 		for (let index = 0; index < json.length; index++) {
 			const objecte = json[index];
-			let factura = new Factura(objecte["num"],new Date(objecte["data"]),objecte["NIF"],objecte["client"],objecte["telefon"],objecte["email"],objecte["descompte"],objecte["IVA"],objecte["pagada"],objecte["adreca"],objecte["poblacio"],objecte["articles"]);
+			let factura = new Factura(objecte["num"], 
+										new Date(objecte["data"]), 
+										objecte["NIF"], 
+										objecte["client"], 
+										objecte["telefon"], 
+										objecte["email"], 
+										objecte["descompte"], 
+										objecte["IVA"], 
+										objecte["pagada"], 
+										objecte["adreca"], 
+										objecte["poblacio"], 
+										objecte["articles"]
+									);
 			Facturas.push(factura);
 			carregarTaula(factura);
 		}
-	};			
+	};
 }
 
-function carregarTaula(factura){
-	function afegirTaula(valor){
+function carregarTaula(factura) {
+	function afegirTaula(valor) {
 		let td = document.createElement("td");
-		let textNode= document.createTextNode(valor);
+		let textNode = document.createTextNode(valor);
 		td.appendChild(textNode);
 		tr.appendChild(td);
 	}
-let taula=document.getElementById("tabla");
-let tr = document.createElement("tr");
+	let taula = document.getElementById("tabla");
+	let tr = document.createElement("tr");
 
-afegirTaula(factura["id"]);
-afegirTaula(factura["data"].toDateString());
-afegirTaula(factura["nif"]);
-afegirTaula(factura["client"]);
-afegirTaula(factura["telefon"]);
-afegirTaula(factura["email"]);
+	afegirTaula(factura["id"]);
+	afegirTaula(factura["data"].toDateString());
+	afegirTaula(factura["nif"]);
+	afegirTaula(factura["client"]);
+	afegirTaula(factura["telefon"]);
+	afegirTaula(factura["email"]);
 
 
-taula.appendChild(tr);
+	taula.appendChild(tr);
 }
 
-function guardar(){
-	
-}
-
-function init(){
+function guardar() {
 
 }
 
-function mostrar(){
+function init() {
 
 }
 
-function afegir(event){
+function mostrar() {
+
+}
+
+function afegir(event) {
 	event.preventDefault();
-	let form=event.target;
-	let id=form[0].value;
-	let fecha=new Date(form[1].value);
-	let pagado=form[2].value=="on"? true : false;
-	let cliente=form[3].value;
-	let nif=form[4].value;
-	let email=form[5].value;
-	let tel=form[6].value;
-	let adreca=form[7].value;
-	let poblacio=form[8].value;
-	let dte=parseFloat(form[9].value/100);
-	let iva=parseFloat(form[10].value/100);
+	let form = event.target;
+	let id = form[0].value;
+	let fecha = new Date(form[1].value);
+	let pagado = form[2].value == "on" ? true : false;
+	let cliente = form[3].value;
+	let nif = form[4].value;
+	let email = form[5].value;
+	let tel = form[6].value;
+	let adreca = form[7].value;
+	let poblacio = form[8].value;
+	let dte = parseFloat(form[9].value / 100);
+	let iva = parseFloat(form[10].value / 100);
 	productosDia.showModal();
-	const productos=[];
-	$("#afegirArticles").on("click",(eve)=>{
+	const productos = [];
+	$("#afegirArticles").on("click", (eve) => {
 		eve.preventDefault()
 		console.log(eve.target.parentElement.children[0].children[1])
 	})
-	let factura=new Factura(id,fecha,nif,cliente,tel,email,dte,iva,pagado,adreca,poblacio)
+	let factura = new Factura(id, fecha, nif, cliente, tel, email, dte, iva, pagado, adreca, poblacio)
 	console.log(factura)
 }
 
-function editar(){
+function editar() {
 
 }
 
-function esborrar(){
+function esborrar() {
 
 }
 
-function imprimir(){
+function imprimir() {
 
 }
-function abrirNuevaFactura(){
+function abrirNuevaFactura() {
 	facturaDia.showModal();
-	let fecha=new Date();
-	$("#id").val(Facturas.length+1);
+	let fecha = new Date();
+	$("#id").val(Facturas.length + 1);
 	//Arreglar mes dia por los 0
-	$("#data").val(`${fecha.getFullYear()}-0${fecha.getMonth()+1}-0${fecha.getDate()}`);
+	$("#data").val(`${fecha.getFullYear()}-0${fecha.getMonth() + 1}-0${fecha.getDate()}`);
 	$("#dte").val(0);
 	$("#iva").val(21);
 }
 
 //Cargar documento JSON desde tu maquina
-document.getElementById("recuperar").addEventListener("change",carregar);
+document.getElementById("recuperar").addEventListener("change", carregar);
 //Descargar documento JSON
-document.getElementById("guardar").addEventListener("click",guardar);
+document.getElementById("guardar").addEventListener("click", guardar);
 //Abrir dialog y añadir productos
-document.getElementById("productos").addEventListener("click",mostrar);
+document.getElementById("productos").addEventListener("click", mostrar);
 //Abrir dialog y rellenar form
-document.getElementById("nuevaFactura").addEventListener("click",abrirNuevaFactura);
+document.getElementById("nuevaFactura").addEventListener("click", abrirNuevaFactura);
 
 //Abrir dialog y editar form ya rellenado con datos
 //document.getElementById("editar").addEventListener("click",editar);
@@ -145,7 +157,7 @@ document.getElementById("nuevaFactura").addEventListener("click",abrirNuevaFactu
 //Imprimir factura
 //document.getElementById("imprimir").addEventListener("click",imprimir);
 
-$("#facturaForm").on("submit",afegir)
+$("#facturaForm").on("submit", afegir)
 
 
 //asdas

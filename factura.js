d166e23aa967e9ad4bbdd3096683b119c073b4d1
rauldmj3 <r.mingo@sapalomera.cat>
2024-02-02 15:49:45 +0100

    Nueva factura

--- factura.js
+++ factura.js
@@ -1,18 +1,28 @@
 "use strict";
 
 //Dialogo
-const factura=document.getElementById("factura");
-const productos=document.getElementById("productos");
-class Factura {
-	constructor() {
-		// Inicialitzar valors
-		this.xPos = 320; // Posició horitzontal de la nau
-		this.yPos = 460; // Posició vertical de la nau
+const facturaDia=document.getElementById("factura");
+const productosDia=document.getElementById("productos");
 
-		// Moure la nau a la posició inicial
-		this.nau = document.getElementById("nau");
-		this.nau.setAttribute("transform", "translate(" + this.xPos + " " + this.yPos + ")");
+//Arrays
+const Facturas=[];
+
+class Factura {
+	constructor(id, data, nif, client, telefon, email, dte, iva, pagat,adreca,poblacio,productos) {
+		this.id= id;
+		this.data= data;
+		this.nif= nif;
+		this.client= client;
+		this.telefon= telefon;
+		this.email= email;
+		this.productos=productos;
+		this.dte= dte;
+		this.iva= iva;
+		this.pagat=pagat;
+		this.adreca=adreca;
+		this.poblacio=poblacio;
 	}
+
 }
 
 function carregar(){
@@ -20,7 +30,7 @@ function carregar(){
 }
 
 function guardar(){
-
+	
 }
 
 function init(){
@@ -31,8 +41,25 @@ function mostrar(){
     console.log(event.target);
 }
 
-function afegir(){
-	factura.showModal();
+function afegir(event){
+	console.log(event)
+	let form=event.target;
+	event.preventDefault();
+	let id=form[0].value;
+	let fecha=new Date(form[1].value);
+	let pagado=form[2].value=="on"? true : false;
+	let cliente=form[3].value;
+	let nif=form[4].value;
+	let email=form[5].value;
+	let tel=form[6].value;
+	let adreca=form[7].value;
+	let poblacio=form[8].value;
+	let dte=parseFloat(form[9].value/100);
+	let iva=parseFloat(form[10].value/100);
+	productosDia.showModal();
+	
+	let factura=new Factura(id,fecha,nif,cliente,tel,email,dte,iva,pagado,adreca,poblacio)
+	console.log(factura)
 }
 
 function editar(){
@@ -46,6 +73,15 @@ function esborrar(){
 function imprimir(){
 
 }
+function abrirNuevaFactura(){
+	facturaDia.showModal();
+	let fecha=new Date();
+	$("#id").val(Facturas.length+1);
+	//Arreglar mes dia por los 0
+	$("#data").val(`${fecha.getFullYear()}-0${fecha.getMonth()+1}-0${fecha.getDate()}`);
+	$("#dte").val(0);
+	$("#iva").val(21);
+}
 
 //Cargar documento JSON desde tu maquina
 document.getElementById("recuperar").addEventListener("click",carregar);
@@ -54,12 +90,16 @@ document.getElementById("guardar").addEventListener("click",guardar);
 //Abrir dialog y añadir productos
 document.getElementById("productos").addEventListener("click",mostrar);
 //Abrir dialog y rellenar form
-document.getElementById("nuevaFactura").addEventListener("click",afegir);
+document.getElementById("nuevaFactura").addEventListener("click",abrirNuevaFactura);
 //Abrir dialog y editar form ya rellenado con datos
-document.getElementById("editar").addEventListener("click",editar);
+//document.getElementById("editar").addEventListener("click",editar);
 //Borrar factura
-document.getElementById("esborrar").addEventListener("click",esborrar);
+//document.getElementById("esborrar").addEventListener("click",esborrar);
 //Imprimir factura
-document.getElementById("imprimir").addEventListener("click",imprimir);
+//document.getElementById("imprimir").addEventListener("click",imprimir);
+
+$("#facturaForm").on("submit",afegir)
+
+
 //asdas
 $(document).ready(init);
